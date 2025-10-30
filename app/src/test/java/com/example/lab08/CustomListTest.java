1af7c31180d6d9321d93bac87c91d40f1d13b713
Momoore Oshinaike <momo.mts06@gmail.com>
2025-10-29 23:33:45 -0600

    added test for deleteCity

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -11,4 +11,14 @@ public class CustomListTest {
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(calgary);
+        list.addCity(edmonton);
+        list.deleteCity(calgary);
+        assertFalse(list.hasCity(calgary));
+    }
 }
